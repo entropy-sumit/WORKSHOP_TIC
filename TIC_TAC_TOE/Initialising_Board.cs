772c0b9e3f2f29f7ef6edf7c012a9df307af231c
entropy-sumit <sumitr600@gmail.com>
2022-01-31 19:00:57 +0530

    [add] sumit

---
+++ TIC_TAC_TOE/Initialising_Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TIC_TAC_TOE
+{
+    class Initialising_Board
+    {
+        public char[] Board()
+        {
+            char[] CreateBoard = new char[10];
+            for (int i = 1; i < CreateBoard.Length; i++)
+            {
+                CreateBoard[i] = ' ';
+            }
+            Console.WriteLine("Board Initialized");
+            return CreateBoard;
+        }
+    }
+}
+
---
+++ TIC_TAC_TOE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TIC_TAC_TOE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
